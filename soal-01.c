576c38189e6c776004c073f768f74f323f77de39
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-22 01:44:29 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -12,19 +12,61 @@
 #include <stdlib.h>
 
 
+int countRec(int uang, int hargaBaru, int hargaBekas, int *tahun);
+int countGpu(int uang, int hargaBaru, int hargaBekas, int *tahun);
+
 int main(){  
+
+	int uang, hargaBaru, hargaBekas;
+	int tahun = 0; 
 
 	// user input
 	printf("Masukkan jumlah uang: ");
-	scanf("%d", ...);
+	scanf("%d", &uang);
 	printf("Masukkan harga beli 1 GPU: ");
-	scanf("%d", ...);
+	scanf("%d", &hargaBaru);
 	printf("Masukkan harga jual 1 GPU: ");
-	scanf("%d", ...);
+	scanf("%d", &hargaBekas);
 
-	//template output
-	printf("\nUang awal harus lebih besar dari harga beli");
-	printf("\nHarga beli harus lebih besar dari harga jual");
-	printf("\nTotal GPU yang bisa dibeli: %d buah\n", ...);
-	printf("Waktu yang dibutuhkan: %d tahun", ...);
+	// exit program jika harga baru > uang awal
+	if (hargaBaru > uang){
+		printf("\nUang awal harus lebih besar dari harga beli");
+		return 0;
+	}
+	
+	// exit program jika harga jual > harga baru
+	if (hargaBekas > hargaBaru){
+		printf("\nHarga beli harus lebih besar dari harga jual");
+		return 0;
+	}
+
+	// panggil fungsi untuk menghitung gpu
+	int gpu = countGpu(uang, hargaBaru, hargaBekas, &tahun);
+
+	// print output
+	printf("\nTotal GPU yang bisa dibeli: %d buah\n", gpu);
+	printf("Waktu yang dibutuhkan: %d tahun", tahun);
+}
+
+int countGpu(int uang, int hargaBaru, int hargaBekas, int *tahun){
+	int gpuCount = uang / hargaBaru;
+	int sisaUang = (uang % hargaBaru) + gpuCount*hargaBekas; 
+
+	// panggil fungsi rekursif
+	return gpuCount + countRec(sisaUang, hargaBaru, hargaBekas, tahun); 
+}
+
+
+int countRec(int uang, int hargaBaru, int hargaBekas, int *tahun){ 
+	// final state
+	if (uang < hargaBaru){
+		return 0;
+	}
+	
+	// recursive state
+	*tahun = *tahun + 2; 
+	int gpuCount = uang / hargaBaru;
+	int sisaUang = (uang % hargaBaru) + gpuCount*hargaBekas; 
+
+	return gpuCount + countRec(sisaUang, hargaBaru, hargaBekas, tahun); 
 }
